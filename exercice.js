8285fb7bf0e03a3da70e5e9bc66bece03b657833
AmauryBerger <amaury.berger.pro@gmail.com>
2020-11-06 18:01:29 +0000

    commit

--- exercice.js
+++ exercice.js
@@ -1,3 +1,83 @@
-var colors;
-colors = ['red', 'yellow', 'green', 'blue'];
-console.log(colors);
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°3
+var myName = 'Amaury';
+console.log(myName);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°4
+var favouriteColor = 'Blue';
+console.log('My favourite color is' + ' ' + favouriteColor);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°5
+var favouriteFood = 'Pizza';
+var city = 'Paris';
+console.log('My favourite meal is' + ' ' + favouriteFood + ' ' + 'and I live in' + ' ' + city);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°6
+var x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log(x);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°7
+var x = 'Laure1';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+//Question N°8 & N°9 = By using console.log, this code just print any message that needs to be displayed by the user when we are executing it.
+console.log(myName + ' ' + favouriteColor + ' ' + favouriteFood);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°10
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°11 
+console.log(phrase + ' ' + one + ', ' + two + ', ' + three + ', ' + four + ', ' + five + ', ' + six + ', ' + seven + ' ' + endingPhrase);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°12
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabet);
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+//Question N°13
+var myAlphabetLength = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabetLength.length); 
+
+console.log(' '); //Space between questions in order to have a better visibility of the answers.
+
+// Question N°14
+var x = 5, y = 10, Z = 15;
+console.log(x + y);
+console.log(y - x);
+console.log(x * y);
+console.log(y / x);
+console.log(x % 2);
+console.log(x++);
+console.log(x--);
